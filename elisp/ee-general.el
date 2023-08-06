78f50f9f35eb7639cf8b2ef4e783bd420918732d
Arnold Hausmann <arnold@pop-os.localdomain>
2023-08-06 17:32:28 -0400

    08/06/2023: worked on General menus & hydras.

--- elisp/ee-general.el
+++ elisp/ee-general.el
@@ -1,7 +1,7 @@
 ;; -*- lexical-binding: t -*-
 ;; File name:     ee-general.el
 ;; Created:       2023-08-05
-;; Last modified: Sat Aug 05, 2023 16:30:18
+;; Last modified: Sun Aug 06, 2023 17:29:09
 ;; Purpose:       Configure the basic General bindings for menu structures to 
 ;;                reduce Hydra usage. Hydra should be used for PERSISTANT menus only.
 ;;
@@ -55,6 +55,37 @@
 
 
 ;; Define main menus for normal/non-normal
+;; Define Buffers Menu.
+(ee-definer
+ "b" '(:ignore t :wk "Buffers")
+ "b b" '(consult-buffer :wk "consult-buffer")
+ "b i" '(ibuffer :wk "Ibuffer")
+ "b k" '(kill-this-buffer :wk "kill buffer")
+ "b e" '(erase-buffer :wk "erase buffer")
+ "b E" '((let ((inhibit-read-only t)) (erase-buffer)) :wk "erase buffer force")
+ "b r" '(rename-buffer :wk "Rename buffer")
+ "b R" '(rename-uniquely :wk "Rename buffer unique")
+ "b g" '(revert-buffer :wk "revert buffer") 
+ "b M" '(aeh/delete-carrage-returns :wk "Delete ^M")
+ "b C" '(ms/cleanup-buffer :wk "Cleanup buffer")
+ )
+
+;; Define Consult Menu.
+(ee-definer
+ "c" '(:ignore t :wk "Consult")
+ "c b" '(consult-bookmark :wk "Bookmarks")
+ "c c" '(consult-mode-command :wk "Mode")
+ "c C" '(consult-minor-mode-menu :wk "Minor mode")
+ "c i" '(consult-imenu :wk "Imenu")
+ "c I" '(consult-project-imenu :wk "Project Imenu")
+ "c m" '(consult-mark :wk "Marks")
+ "c M" '(consult-global-mark :wk "Global marks")
+ "c r" '(consult-recent-file :wk "recent files")
+ "c o" '(consult-outline :wk "Outline")
+ "c t" '(consult-theme :wk "Themes")
+ "c y" '(consult-yank-from-kill-ring :wk "Yank from kill ring")
+)
+
 ;; Define Files Menu.
 (ee-definer
  "f" '(:ignore t :wk "Files")
@@ -72,9 +103,59 @@
  "f U" '(aeh/set-buffer-to-unix-format :wk "Convert to UNIX")
  )
 
+;; Define Modes Menu.
+(ee-definer
+ "m" '(:ignore t :wk "Modes")
+ "m o" '(orgtbl-mode :wk "Org Table mode")
+ "m p" '(prog-mode :wk "Prog mode")
+ "m s" '(sql-mode :wk "SQL mode")
+ "m t" '(text-mode :wk "Text mode")
+)
 
+;; Define Toggles Menu.
+(ee-definer
+ "t" '(:ignore t :wk "Toggles")
+ "t a" '(aggressive-indent-mode :wk "Aggressive indent")
+ "t b" '(page-break-lines-mode :wk "Page break lines")
+ "t c" '(column-enforce-mode :wk "Col. enforce")
+ "t C" '(company-mode :wk "Company mode")
+ "t D" '(drag-stuff-mode :wk "Drag stuff") 
+ "t e" '(toggle-debug-on-error :wk "Debug on error")
+ "t g" '(toggle-debug-on-quit :wk "Debug on quit")
+ "t h" '(highlight-thing-mode :wk "Highlight thing")
+ "t l" '(display-line-numbers-mode :wk "Line numbers")       ; display type set to "visual" in "Better-defaults"
+ "t p" '(smartparens-mode :wk "SmartParens")
+ "t r" '(read-only-mode :wk "Read-only")
+ "t f" '(auto-fill-mode :wk "Auto fill")
+ "t s" '(flycheck-mode :wk "Flycheck")
+ "t S" '(flyspell-mode :wk "Flyspell")
+ "t t" '(toggle-truncate-lines :wk "Truncate Lines")
+ "t w" '(whitespace-mode :wk "White space")
+ "t W" '(toggle-word-wrap :wk "Word wrap")
+ "t z" '(origami-mode :wk "Origami")
+)
 
+;; Define Windows Menu.
+(ee-definer
+ "w" '(:ignore t :wk "Windows")
+ "w b" '(balance-windows :wk "Balance windows")
+ "w h" '(enlarge-window-horizontally :wk "Grow horizontal")
+ "w j" '(enlarge-window :wk "Grow vertical")
+ "w k" '(shrink-window :wk "Shrink vertical")
+ "w l" '(shrink-window-horizontally :wk "Shrink horizontal")
+ "w t" '(text-scale-adjust :wk "Text adjust scale")
+)
 
+;; Define Yasnippets Menu.
+(ee-definer
+ "y" '(:ignore t :wk "Yasnippets")
+ "y d" '(yas-describe-tables :wk "Describe tables")
+ "y i" '(yas-insert-snippet :wk "Insert snippet")
+ "y l" '(yas-load-directory :wk "Load directory")
+ "y n" '(yas-new-snippet :wk "New snippet")
+ "y r" '(yas-reload-all :wk "Reload all")
+ "y v" '(yas-visit-snippet-file :wk "Visit snippet")
+)
 
 ;; Ref: https://github.com/progfolio/.emacs.d#general-key-bindings
 ;; 08/05/2023: don't really like any of these, and "zoom" doesn't work
